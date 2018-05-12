735fc47b0cfce4959ec30a11a21983b5c2d5431e
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-05-12 22:45:49 +0530

    Program of creating queue using circular linked list

---
+++ C/Queue/Using Linked List/Circular.c
@@ -0,0 +1,106 @@
+/* Program of queue using circular linked list*/
+# include <stdio.h>
+# include <malloc.h>
+
+struct node
+{
+	int info;
+	struct node *link;
+}*rear=NULL;
+
+int insert();
+int delete();
+int display();
+
+int main()
+{
+	int choice;
+	while(1)
+	{
+		printf("1.Insert \n");
+		printf("2.Delete \n");
+		printf("3.Display\n");
+		printf("4.Quit\n");
+		printf("Enter your choice : ");
+		scanf("%d",&choice);
+
+		switch(choice)
+		{
+		 case 1:
+			insert();
+			break;
+		 case 2:
+			del();
+			break;
+		 case 3:
+			display();
+			break;
+		 case 4:
+			return(0);
+		 default:
+			printf("Wrong choice\n");
+		}/*End of switch*/
+	}/*End of while*/
+}/*End of main()*/
+
+int insert()
+{
+	int num;
+	struct node *tmp;
+	printf("Enter the element for insertion : ");
+	scanf("%d",&num);
+	tmp= malloc(sizeof(struct node));
+	tmp->info = num;
+
+	if(rear == NULL) /*If queue is empty */
+	{
+		rear = tmp;
+		tmp->link = rear;
+	}
+	else
+	{
+		tmp->link = rear->link;
+		rear->link = tmp;
+		rear = tmp;
+	}
+}/*End of insert()*/
+
+int del()
+{
+	struct node *tmp,*q;
+	if(rear==NULL)
+	{
+		printf("Queue underflow\n");
+		return;
+	}
+	if( rear->link == rear )  /*If only one element*/
+	{
+		tmp = rear;
+		rear = NULL;
+		free(tmp);
+		return;
+	}
+	q=rear->link;
+	tmp=q;
+	rear->link = q->link;
+	printf("Deleted element is %d\n",tmp->info);
+	free(tmp);
+}/*End of del()*/
+
+int display()
+{
+	struct node *q;
+	if(rear == NULL)
+	{
+		printf("Queue is empty\n");
+		return;
+	}
+	q = rear->link;
+	printf("Queue is :\n");
+	while(q != rear)
+	{
+		printf("%d ", q->info);
+		q = q->link;
+	}
+	printf("%d\n",rear->info);
+}/*End of display()*/
